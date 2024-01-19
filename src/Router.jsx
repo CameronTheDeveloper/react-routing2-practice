874fc42051b425073631023a79c9aef5e11ff523
Cameron Posey <cameronposey@outlook.com>
2024-01-19 01:13:12 -0800

    Add ErrorPage as root errorElement

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,14 +2,15 @@ import {
     createBrowserRouter,
     RouterProvider
 } from "react-router-dom";
-
 import Root from "./routes/Root";
+import ErrorPage from "./ErrorPage";
 
 function Router(){
     const browserRouter = createBrowserRouter([
         {
             path: '/',
-            element: <Root></Root>
+            element: <Root></Root>,
+            errorElement: <ErrorPage></ErrorPage>,
         }
     ])
 
